3f97ddc1d51070477eacd8cc1dbd821e41906c3d
maxikgreat <maximvasylenko228322@gmail.com>
2020-04-05 13:03:50 +0200

    Uncomment

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,81 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import {Link, NavLink} from 'react-router-dom'
 import LogoItem from '../assets/img/home/logo.png'
+import {Navbar, Nav} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Header = () => {
-    return(
-        <header className="header">
-            <div className="header__content">
-                <div className="header__logo">
-                    <div className="header__logo-img">
-                        <NavLink to="/"><img src={LogoItem} alt="" /></NavLink>
+
+
+    const [mobileActive, setMobile] = useState(false);
+
+    const toggleMobileNav = () => {
+        setMobile(!mobileActive);
+    };
+
+    const renderMobileNav = () => {
+        return (
+            <div className='mobile-nav-container'>
+                <div className="mobile-nav">
+                    <div className="title-container">
+                        <FontAwesomeIcon icon={'times-circle'} onClick = {() => {toggleMobileNav()}}/>
                     </div>
-                    <div className="header__logo-name">
-                        <NavLink to="/" className="logoname-1" id="ln1">Create</NavLink>
-                        <NavLink to="/" className="logoname-2" id="ln2"> Query</NavLink>
+                    <div className='nav-links-container'>
+
+                        <NavLink to='/services' className={'header__button'}>Услуги</NavLink>
+                        <NavLink to='/about'>О нас</NavLink>
+                        <NavLink to='/contacts'>Контакты</NavLink>
+                    </div>
+                    <hr />
+                    <div className='additional-options-container'>
+                        <div className="header__phone"><a href="tel:+380509834900">+380 50 98 34 900</a></div>
+                        <div className="header__button"><a href={"#"}>Оформить заказ</a></div>
+                    </div>
+                    <hr />
+                    <div className='footer-menu-container'>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi blanditiis corporis distinctio itaque iusto nisi, recusandae reiciendis. Debitis, esse.</p>
                     </div>
                 </div>
-
-                <div className="header__menu">
-                    <nav className="menu">
-                        <div className="header__btn"><span className="icon-bars"></span></div>
-                        <ul>
-                            <li><NavLink to='/about'>О компании</NavLink></li>
-                            <li><NavLink to="/service">Услуги</NavLink></li>
-                            <li><a href="#">Контакты</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div className="header__phone"><a href="">+380 50 98 34 900</a></div>
-
-                <div className="header__button"><a href="#">Оформить заказ</a></div>
             </div>
+        )
+    }
+
+
+    return(
+        <header>
+            <Navbar expand="lg" bg={'primary-dark'}>
+                <Navbar.Brand>
+                        <Link to = '/' className="header__logo">
+                            <div className="header__logo-img">
+                                <img src={LogoItem} alt="" />
+                            </div>
+                            <div className="header__logo-name">
+                                <span className="logoname-1" id="ln1">Create</span>
+                                <span to="/" className="logoname-2" id="ln2"> Query</span>
+                            </div>
+                        </Link>
+                </Navbar.Brand>
+                <FontAwesomeIcon
+                    icon={'bars'}
+                    className = 'menu-toggler'
+                    onClick = {() => {toggleMobileNav()}}
+                />
+                {mobileActive ? renderMobileNav() : null}
+                <Navbar.Collapse>
+                    <Nav className="nav-container">
+                        <div className='nav-links-container d-flex flex-row'>
+                                <NavLink to='/services'>Услуги</NavLink>
+                                <NavLink to='/about'>О нас</NavLink>
+                                <NavLink to='/contacts'>Контакты</NavLink>
+                        </div>
+                        <div>
+                            <div className="header__phone"><a href="tel:+380509834900">+380 50 98 34 900</a></div>
+                            <div className="header__button"><a href={"#"}>Оформить заказ</a></div>
+                        </div>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </header>
     )
 };
